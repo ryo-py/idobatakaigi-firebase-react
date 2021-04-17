f7aef49565e33006d468ea292f638caa963487ab
Ryo <ryo91.hb@gmail.com>
2021-04-17 23:52:34 +0900

    fix keydown event preventdefault

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -71,6 +71,13 @@ export default function SignIn({ setName }) {
             name="email"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              console.log(e.key);
+              if (e.key === "Enter") {
+                setName(e.target.value);
+                e.preventDefault();
+              }
+            }}
           />
           <Button
             type="button"
