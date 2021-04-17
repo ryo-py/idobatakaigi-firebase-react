876c8678b42baca31658173eb255a5029df5ccee
Ryo <ryo91.hb@gmail.com>
2021-04-17 21:20:38 +0900

    create sign in page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React from "react";
+import SignIn from "./Signin";
 
 export default () => {
-  return <div>Hello, React!</div>;
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
 };
