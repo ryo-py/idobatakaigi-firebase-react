11ab84443c26a2d96f7c2a93bd3c04956e283e83
Ryo <ryo91.hb@gmail.com>
2021-04-19 18:30:46 +0900

    message push to firebase

--- src/components/MessagerField.js
+++ src/components/MessagerField.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 
+import { pushMessage } from "../firebase";
+
 const MessagerField = ({ name, text, setText }) => {
   const [isComposed, setIsComposed] = useState(false);
   console.log("text: ", text);
@@ -15,7 +17,7 @@ const MessagerField = ({ name, text, setText }) => {
         const text = e.target.value;
         if (text === "") return;
         if (e.key === "Enter") {
-          console.log("push message to firebase", text);
+          pushMessage({ name: "ryo", text });
           setText("");
           e.preventDefault();
         }
